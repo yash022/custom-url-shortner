9ee5c1e4f44f19dd87eb24121eecee0af576839b
Yash Sehgal <96070479+yash022@users.noreply.github.com>
2022-01-31 10:39:01 +0530

    Update mainapi.py

--- mainapi.py
+++ mainapi.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/<string:id>')
 def short(id):
-    data = requests.get("https://docs.google.com/spreadsheets/d/1QAUlWAbMRemISJJe0Ujjk0vdtz5oq67ZOG-GWcRlcII/gviz/tq?tqx=out:csv&tq=SELECT *")
+    data = requests.get("https://docs.google.com/spreadsheets/d/<google sheet id>/gviz/tq?tqx=out:csv&tq=SELECT *")
     data_format = data.text.split("\n")
     data_final = [i.split(",") for i in data_format]
     ids = []
