67fa466d4715397d6d8d2a96c97fa9b99a660b86
Yash Sehgal <96070479+yash022@users.noreply.github.com>
2022-01-31 10:38:28 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import requests
 
 def check(name):
-    data = requests.get("https://docs.google.com/spreadsheets/d/1QAUlWAbMRemISJJe0Ujjk0vdtz5oq67ZOG-GWcRlcII/gviz/tq?tqx=out:csv&tq=SELECT *")
+    data = requests.get("https://docs.google.com/spreadsheets/d/<google sheet id>/gviz/tq?tqx=out:csv&tq=SELECT *")
     data_format = data.text.split("\n")
     data_final = [i.split(",") for i in data_format]
     ids = []
@@ -16,13 +16,13 @@ def check(name):
 st.title("Yash's URL Shortner")
 url_textbox = st.text_input("Enter URL to be shortened",placeholder="url")
 Id_textbox = st.text_input("Enter Id you want",placeholder="Id")
-post_db = f"https://script.google.com/macros/s/AKfycbyO28VlEsqwwMWILb687tUg0-mmoAmGAQpd1Kat18vVoICpMy-KrXoitHYxLjpcdRLktQ/exec?url={url_textbox}&Unique_id={Id_textbox}"
+post_db = f"https://script.google.com/macros/s/<app script web app>/exec?url={url_textbox}&Unique_id={Id_textbox}"
 if st.button("Shorten"):
     if check(Id_textbox) == "True":
         st.error("choose another Id")
     else:
         requests.get(post_db)
-        shortned_url = f"https://shortopop.herokuapp.com/{Id_textbox}"
+        shortned_url = f"https://<heroku api app name>.herokuapp.com/{Id_textbox}"
         st.success("Your URL has been shortened")
         st.write(f"Your shortened URL is: {shortned_url}")
 
